4d60e4cc2884db5505ca5430b5253953336dbd4f
rvsmooth <riveeks.smooth@gmail.com>
2025-08-23 21:59:56 +0530

    chore: fixes

--- setup.bat
+++ setup.bat
@@ -17,12 +17,6 @@ set "installer=%currentDir%\vscode.exe"
 "%installer%" /silent
 echo Done
 
-call %currentDir%\refrenv.bat
-
-echo Installing C/C++ extension for VSCode
-set "extension=%currentDir%\c.vsix"
-code --install-extension "%extension%"
-echo Done
 
 echo Adding compiler to PATH
 setx PATH "%PATH%;C:\mingw64\bin" /M
