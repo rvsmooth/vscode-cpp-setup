947871d2d5195aa833f98a353a7fec35c9dae838
rvsmooth <riveeks.smooth@gmail.com>
2025-08-23 20:05:10 +0530

    chore: update

--- setup.bat
+++ setup.bat
@@ -1,18 +1,24 @@
 @echo off
+
+set "currentDir=%~dp0%"
 set "zipFile=winlibs.zip"
 set "destFolder=C:\"
 
-powershell -Command "Expand-Archive -Path '%zipFile%' -DestinationPath '%destFolder%' -Force"
+powershell -Command "Expand-Archive -Path '$currentDir%\%zipFile%' -DestinationPath '%destFolder%' -Force"
 
 echo Done
 
-set "installer=vscode.exe"
+echo Installing VSCode
+set "installer=$currentDir\vscode.exe"
 "%installer%" /silent
+echo Done
 
-
-set "extension=c.vsix"
+echo Installing C/C++ extension for VSCode
+set "extension=%currentDir%\c.vsix"
 code --install-extension "%extension%"
+echo Done
 
+echo Adding compiler to PATH
+setx PATH "%PATH%;C:\mingw64\bin" /M
+echo Done
 pause
-
-
