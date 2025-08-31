3e38f30a284bf613802efcd82daef78d67ac650b
rvsmooth <riveeks.smooth@gmail.com>
2025-08-31 10:51:13 +0530

    chore: update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ script_dir = os.path.abspath(os.path.dirname(sys.argv[0]))
 sevenzip_path = os.path.join(script_dir, "7zip.exe")
 winlibs_path = os.path.join(script_dir, "winlibs.zip")
 vs_code_path = os.path.join(script_dir, "vscode.exe")
+gcc_path = os.path.abspath(os.path.join(os.environ["HOMEDRIVE"], "mingw64", "bin"))
 
 cpp_dir = os.path.join(script_dir, "cpptools.vsix")
 cmd = [vs_code_path, "/VERYSILENT", "/NORESTART", '/MERGETASKS="!runcode"']
@@ -22,9 +23,14 @@ sevenzip_executable = os.path.abspath(
 )
 
 
-def run_cmd(path_to_exec, args, args_2nd, args_3rd):
+def run_cmd(path_to_exec, args, args_2nd=None, args_3rd=None):
     args = shlex.split(args)
-    cmd = [path_to_exec] + args
+    if path_to_exec and args:
+        cmd = [path_to_exec] + args
+    if path_to_exec and args and args_2nd:
+        cmd = [path_to_exec] + args + [args_2nd]
+    if path_to_exec and args and args_2nd and args_3rd:
+        cmd = [path_to_exec] + args + [args_2nd] + [args_3rd]
     subprocess.run(cmd)
 
 
@@ -42,4 +48,14 @@ print("Installing 7zip")
 run_cmd(sevenzip_path, "/S")
 
 print("Extracting winlibs")
-run_cmd(sevenzip_executable, "x", winlibs_path, "-oC:")
+if os.path.exists(gcc_path):
+    print("GCC is already installed")
+else:
+    run_cmd(sevenzip_executable, "x", winlibs_path, "-oC:")
+
+print("Adding mingw64 to path")
+if shutil.which("gcc"):
+    print("compiler is already in path")
+else:
+    argument = "PATH %PATH%;" + gcc_path
+    run_cmd("setx", argument)
